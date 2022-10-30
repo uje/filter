b8a778d118653d9056c1214e4a04b9b30e983b92
Yuanhe <4176641+uje@users.noreply.github.com>
2022-10-30 09:20:48 +0000

    1111

--- app.js
+++ app.js
@@ -9,7 +9,12 @@ const app = express();
 app.use((request, response, next) => {
   const wd = decodeURIComponent(request.query.wd);
 
-  if (/\/\s$/.test(request.path) === false || ['间谍', '监听', '监视', '组织', '跟踪', '尾随'].includes(wd) === false) {
+  if (/\/\s$/.test(request.path) === false) {
+    forward(`https://www.baidu.com/${request.url}`)(request, response, next);
+    return;
+  }
+
+  if (['间谍', '监听', '监视', '组织', '跟踪', '尾随'].includes(wd) === false) {
     forward(`https://www.baidu.com/${request.url}`)(request, response, next);
     return;
   }
