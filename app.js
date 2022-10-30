36524bb1144c68a6cf0f1cea199a0c85f5d4b254
Yuanhe <4176641+uje@users.noreply.github.com>
2022-10-30 07:44:08 +0000

    增加文件

---
+++ app.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+const express = require("express");
+const app = express();
+
+app.use((request, response, next) => {
+  if (req.path !== "/s") {
+    return next();
+  }
+
+  response.send('ok');
+});
+
+app.listen("10086", () => {
+  console.log("服务运行在80端口");
+});
