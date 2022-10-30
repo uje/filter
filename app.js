8accfc4ff3ff92e91d6fb695827b83dc51382ca7
Yuanhe <4176641+uje@users.noreply.github.com>
2022-10-30 09:05:01 +0000

    1111

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
 const express = require("express");
 const forward = require('express-http-proxy');
 const app = express();
 
 app.use((request, response, next) => {
-  if (request.path !== "/s") {
+  if (/\/s$/.test(request.path) === false) {
     forward(`https://www.baidu.com/${request.url}`)(request, response, next);
     return;
   }
 
-  response.send(request.query.wd);
+  const ua = request.headers['user-agent'];
+  const isMobile = /ipad|iphone|android|mobile/.test(ua);
+  const template = isMobile ? 'mobile.html' : 'pc.html'
+  const content = fs.readFileSync(path.join(__dirname, `./template/${template}`));
+
+  response.send(content);
 });
 
 app.listen("10086", () => {
