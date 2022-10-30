43e59fbfc21cb093113044448c68eb9e2f63c3a4
Yuanhe <4176641+uje@users.noreply.github.com>
2022-10-30 09:25:53 +0000

    1111

--- app.js
+++ app.js
@@ -9,9 +9,6 @@ const app = express();
 app.use((request, response, next) => {
   const wd = decodeURIComponent(request.query.wd || request.query.word);
 
-  response.send(`isPath: ${request.path.endsWith('/s')}, path: ${request.path}, isKey: ${['间谍', '监听', '监视', '组织', '跟踪', '尾随'].includes(wd) === false}, wd: ${wd}`);
-  return;
-
   if (request.path.endsWith('/s') === false) {
     forward(`https://www.baidu.com/${request.url}`)(request, response, next);
     return;
